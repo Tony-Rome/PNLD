7d46d1116b3b6084ea7a393b64fb823f21da4ee7
Tony-Rome <tonyn.rome@gmail.com>
2020-12-28 15:15:56 -0300

    Reestrutura de archetipo
    Se agrega carpeta resource/ y archivo .properties

--- src/main/java/application/PNLDApplication.java
+++ src/main/java/application/PNLDApplication.java
@@ -1,4 +1,4 @@
-package main.webapp.application;
+package main.java.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
