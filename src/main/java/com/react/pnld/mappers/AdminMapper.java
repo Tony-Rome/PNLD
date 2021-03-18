f76eb4a58512e9c164f0d5329c1d44584cbba2aa
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2021-03-18 18:22:13 -0300

    Comentario borrado

--- src/main/java/com/react/pnld/mappers/AdminMapper.java
+++ src/main/java/com/react/pnld/mappers/AdminMapper.java
@@ -5,18 +5,6 @@ import org.apache.ibatis.annotations.Select;
 
 public interface AdminMapper {
 
-    /**
-     *  Modelo de datos Admin tiene las siguientes columnas:
-     *    Column  |       Type        | Collation | Nullable |              Default
-     * ----------+-------------------+-----------+----------+-----------------------------------
-     *  id       | integer           |           | not null | nextval('admin_id_seq'::regclass)
-     *  username | character varying |           | not null |
-     *  email    | character varying |           | not null |
-     *  password | character varying |           | not null |
-     *  role     | character varying |           | not null |
-     *
-     *  valor del campo role debe ser ROLE_ADMIN
-     * */
     @Select("SELECT * FROM pnld.pnld.admin WHERE nombre = #{nombre}")
     Admin findByUsername(String nombre);
 }
