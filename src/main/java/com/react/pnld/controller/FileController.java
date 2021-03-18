4f2a936d7ed9653f20902ae6dd664b5ae385bfb2
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2021-03-17 22:53:56 -0300

    conflicts solved

--- src/main/java/com/react/pnld/controller/FileController.java
+++ src/main/java/com/react/pnld/controller/FileController.java
@@ -2,6 +2,8 @@ package com.react.pnld.controller;
 
 import com.react.pnld.model.CsvFile;
 import com.react.pnld.service.FileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +14,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileController {
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
     @Autowired
     private FileService fileService;
 
     @GetMapping(value = "/")
     public String index(Model model) {
-
+        logger.debug("scheduleFileLoadGet. form load file is loaded");
         return "index";
     }
 
