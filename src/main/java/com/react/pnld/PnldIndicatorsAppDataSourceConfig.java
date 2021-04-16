c0435856c10bd0627ba056beee3613a30c103d80
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2021-04-15 20:56:39 -0400

    Application limpiado

--- src/main/java/com/react/pnld/PnldIndicatorsAppDataSourceConfig.java
+++ src/main/java/com/react/pnld/PnldIndicatorsAppDataSourceConfig.java
@@ -1,14 +1,17 @@
 package com.react.pnld;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-
 import javax.sql.DataSource;
 
 @Configuration
+@MapperScan("com.react.pnld.mappers")
 public class PnldIndicatorsAppDataSourceConfig {
 
     @Autowired
     private DataSource dataSource;
+
+
 
 }
--- src/main/java/com/react/pnld/PnldIndicatorsApplication.java
+++ src/main/java/com/react/pnld/PnldIndicatorsApplication.java
@@ -1,31 +1,16 @@
 package com.react.pnld;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.react.pnld.model")
-@MapperScan("com.react.pnld.mappers")
 @EnableScheduling
 public class PnldIndicatorsApplication {
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource);
-        return sqlSessionFactoryBean.getObject();
-    }
 
     public static void main(String[] args) { SpringApplication.run(PnldIndicatorsApplication.class, args); }
 
