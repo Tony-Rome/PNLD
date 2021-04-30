e45e239607610401b4412aa42ff1999c7e4e4892
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2021-04-30 14:47:30 -0400

    indicador vista base

---
+++ src/main/java/com/react/pnld/controller/IndicatorController.java
@@ -0,0 +1,14 @@
+package com.react.pnld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndicatorController {
+
+
+    @GetMapping("/indicators")
+    public String home(){
+        return "indicator";
+    }
+}
