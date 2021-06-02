1d98d339fb376376477bb685fa712835a9d5d96d
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2021-06-02 13:07:19 -0400

    seleccion todos los filtros

--- src/main/resources/static/js/indicator/training/filter.js
+++ src/main/resources/static/js/indicator/training/filter.js
@@ -1,6 +1,11 @@
 const yearList = document.getElementsByName('year');
 const regionList = document.getElementsByName('region');
 const genderList = document.getElementsByName('gender');
+const allYear = document.getElementById('allYear');
+const allRegion = document.getElementById('allRegion');
+const allGender = document.getElementById('allGender');
+
+
 
 getYearsSelected = () => {
     const yearsFilters = document.getElementsByName('year');
@@ -32,6 +37,25 @@ getGendersSelected = () => {
     console.log(gendersSelected);
 };
 
+function selectAllYears() {
+
+    yearList.forEach( (e, i) => {
+        e.checked = this.checked;
+    })
+};
+
+function selectAllRegions() {
+    regionList.forEach( (e, i) => {
+        e.checked = this.checked;
+    })
+};
+
+function selectAllGenders() {
+    genderList.forEach( (e,i) => {
+        e.checked = this.checked;
+    })
+};
+
 yearList.forEach((e,i) => {
     e.addEventListener('click', getYearsSelected);
 });
@@ -43,3 +67,7 @@ regionList.forEach((e,i) => {
 genderList.forEach((e,i) => {
     e.addEventListener('click', getGendersSelected);
 });
+
+allYear.addEventListener('change', selectAllYears);
+allRegion.addEventListener('click', selectAllRegions);
+allGender.addEventListener('click', selectAllGenders);
