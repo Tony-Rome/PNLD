9bfce8196b06feddd776f35eb48ca7ec4c7fd743
=Antonio Rodríguez Mejías <tonyn.rome@gmail.com>
2021-06-11 13:52:59 -0400

    Cambio colores, boton home

--- src/main/resources/static/js/indicator/training/chart.js
+++ src/main/resources/static/js/indicator/training/chart.js
@@ -138,6 +138,10 @@ export function getPercentageBarChart (labels, datasets, title, dataList) {
                   display: true,
                   text: '% de establecimientos que participan por primera vez ' + defineTitle(title),
               },
+
+
+
+
               legend: {
                   display: true
               }
